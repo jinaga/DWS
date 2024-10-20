8506c658b1c14faf822c6b8948db4acc0875b046
Michael L Perry <michael@qedcode.com>
2024-10-20 12:59:59 -0500

    Capture the seleted tool in the TaskToolViewModel

--- DWS.Console/Areas/Tasks/TaskToolViewModel.cs
+++ DWS.Console/Areas/Tasks/TaskToolViewModel.cs
@@ -1,7 +1,12 @@
-﻿namespace DWS.Console.Areas.Tasks;
+﻿using DWS.Model;
+
+namespace DWS.Console.Areas.Tasks;
 
 public partial class TaskToolViewModel : ObservableObject
 {
     [ObservableProperty]
     private string name = string.Empty;
+
+    [ObservableProperty]
+    private Tool? tool;
 }
